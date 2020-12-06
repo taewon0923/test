acdc7c1fdeda42304dbc235ca2cacbf964f9afae
ByeonGooHoon <dksekfldks65@naver.com>
2020-12-06 12:43:25 +0900

    메인페이지 페이징, 이미지 불러올경우 permitAll 설정

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         ;
 
         http.authorizeRequests()
-                .mvcMatchers("/", "/members/**", "/item/**").permitAll()
+                .mvcMatchers("/**", "/members/**", "/item/**", "/images/**").permitAll()
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
         ;
