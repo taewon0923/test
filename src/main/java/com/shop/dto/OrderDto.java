ec5a21a94ec35120aacb2634545502f0f38e13f3
ByeonGooHoon <dksekfldks65@naver.com>
2020-12-21 18:34:54 +0900

    주문 최대 수량 validation 추가

--- src/main/java/com/shop/dto/OrderDto.java
+++ src/main/java/com/shop/dto/OrderDto.java
@@ -3,6 +3,7 @@ package com.shop.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
@@ -13,6 +14,7 @@ public class OrderDto {
     private Long itemId;
 
     @Min(value = 1, message = "최소 주문 수량은 1개 입니다.")
+    @Max(value = 999, message = "최대 주문 수량은 999개 입니다.")
     private int count;
 
 }
