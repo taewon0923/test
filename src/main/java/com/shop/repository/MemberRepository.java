ee974d92f688b7e0366e3f7e4cc7921330026768
ByeonGooHoon <dksekfldks65@naver.com>
2020-12-21 16:37:40 +0900

    @Repository 어노테이션 삭제

--- src/main/java/com/shop/repository/MemberRepository.java
+++ src/main/java/com/shop/repository/MemberRepository.java
@@ -2,9 +2,7 @@ package com.shop.repository;
 
 import com.shop.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByEmail(String email);
