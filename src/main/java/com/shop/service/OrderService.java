6162851505fbaeb49ec99ebcf4d64a928ee243c3
ByeonGooHoon <dksekfldks65@naver.com>
2020-12-23 21:01:12 +0900

    주문 이력 조회 구현

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.shop.service;
 
 import com.shop.dto.OrderDto;
+import com.shop.dto.OrderHistDto;
+import com.shop.dto.OrderItemDto;
 import com.shop.entity.Item;
 import com.shop.entity.Member;
 import com.shop.entity.Order;
@@ -9,6 +11,9 @@ import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
 import com.shop.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,4 +45,25 @@ public class OrderService {
         return order.getId();
     }
 
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+
+        List<Order> orders  = orderRepository.findOrders(email, pageable);
+        Long totalCount = orderRepository.countOrder(email);
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        for (Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            for (OrderItem orderItem : orderItems) {
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem);
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
+    }
 }
